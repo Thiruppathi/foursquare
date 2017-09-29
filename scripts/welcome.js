8abf1b20d22b1e15ebd738f2923512a823b5fcad
dushyant.sabharwal <dushyant.sabharwal@servicenow.com>
2017-09-29 17:46:48 +0200

    handling url rewriting in js

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
 	function prepareRegisterLink(appConfig) {
-		return appConfig.registerUrl + "&client_id=" + appConfig.clientId + "&redirect_uri=" + window.location.origin + appConfig.redirectUrlPath;
+		var path = window.location.pathname.split("/").slice(1);
+		// remove the current name of the page.
+		path.pop();
+		if (path.length > 0) {
+			path = path.join("/");
+		}
+
+		return appConfig.registerUrl + "&client_id=" + appConfig.clientId + "&redirect_uri=" + window.location.origin + path + appConfig.redirectUrlPath;
 	}
 
 	fetchConfigService.get().done(function(appConfig) {	
